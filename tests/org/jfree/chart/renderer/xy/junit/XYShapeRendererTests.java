5a8d9dbf156eac8640f20b51c5a9fbadeb804711
Martin Krauskopf <martin.krauskopf@gmail.com>
2011-10-05 12:13:42 +0200

    Original files to be patched.

---
+++ tests/org/jfree/chart/renderer/xy/junit/XYShapeRendererTests.java
@@ -0,0 +1,199 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2010, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * -------------------------
+ * XYShapeRendererTests.java
+ * -------------------------
+ * (C) Copyright 2010, by Object Refinery Limited.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   Martin Hoeller (patch 2952086);
+ *
+ * Changes
+ * -------
+ * 17-Sep-2008 : Version 1 (DG);
+ * 16-Feb-2010 : Added testFindZBounds() (MH);
+ *
+ */
+
+package org.jfree.chart.renderer.xy.junit;
+
+import java.awt.Color;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.jfree.chart.renderer.LookupPaintScale;
+import org.jfree.chart.renderer.xy.XYShapeRenderer;
+import org.jfree.data.Range;
+import org.jfree.data.xy.DefaultXYZDataset;
+import org.jfree.data.xy.XYZDataset;
+
+/**
+ * Tests for the {@link XYShapeRenderer} class.
+ */
+public class XYShapeRendererTests extends TestCase {
+
+    /**
+     * Returns the tests as a test suite.
+     *
+     * @return The test suite.
+     */
+    public static Test suite() {
+        return new TestSuite(XYShapeRendererTests.class);
+    }
+
+    /**
+     * Constructs a new set of tests.
+     *
+     * @param name  the name of the tests.
+     */
+    public XYShapeRendererTests(String name) {
+        super(name);
+    }
+
+    /**
+     * Some checks for the equals() method.
+     */
+    public void testEquals() {
+        XYShapeRenderer r1 = new XYShapeRenderer();
+        XYShapeRenderer r2 = new XYShapeRenderer();
+        assertTrue(r1.equals(r2));
+        assertTrue(r2.equals(r1));
+
+        r1.setPaintScale(new LookupPaintScale(1.0, 2.0, Color.white));
+        assertFalse(r1.equals(r2));
+        r2.setPaintScale(new LookupPaintScale(1.0, 2.0, Color.white));
+        assertTrue(r1.equals(r2));
+
+        r1.setDrawOutlines(true);
+        assertFalse(r1.equals(r2));
+        r2.setDrawOutlines(true);
+        assertTrue(r1.equals(r2));
+
+        r1.setUseOutlinePaint(false);
+        assertFalse(r1.equals(r2));
+        r2.setUseOutlinePaint(false);
+        assertTrue(r1.equals(r2));
+
+        r1.setUseFillPaint(true);
+        assertFalse(r1.equals(r2));
+        r2.setUseFillPaint(true);
+        assertTrue(r1.equals(r2));
+
+        r1.setGuideLinesVisible(true);
+        assertFalse(r1.equals(r2));
+        r2.setGuideLinesVisible(true);
+        assertTrue(r1.equals(r2));
+
+        r1.setGuideLinePaint(Color.red);
+        assertFalse(r1.equals(r2));
+        r2.setGuideLinePaint(Color.red);
+        assertTrue(r1.equals(r2));
+
+    }
+
+    /**
+     * Confirm that cloning works.
+     */
+    public void testCloning() {
+        XYShapeRenderer r1 = new XYShapeRenderer();
+        XYShapeRenderer r2 = null;
+        try {
+            r2 = (XYShapeRenderer) r1.clone();
+        }
+        catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(r1 != r2);
+        assertTrue(r1.getClass() == r2.getClass());
+        assertTrue(r1.equals(r2));
+    }
+
+    /**
+     * Serialize an instance, restore it, and check for equality.
+     */
+    public void testSerialization() {
+        XYShapeRenderer r1 = new XYShapeRenderer();
+        XYShapeRenderer r2 = null;
+        try {
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            ObjectOutput out = new ObjectOutputStream(buffer);
+            out.writeObject(r1);
+            out.close();
+
+            ObjectInput in = new ObjectInputStream(
+                    new ByteArrayInputStream(buffer.toByteArray()));
+            r2 = (XYShapeRenderer) in.readObject();
+            in.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertEquals(r1, r2);
+    }
+
+    /**
+     * Check if finding the bounds in z-dimension of an XYZDataset works. 
+     */
+    public void testFindZBounds() {
+        XYShapeRenderer r = new XYShapeRenderer();
+        assertNull(r.findZBounds(null));
+        
+        DefaultXYZDataset dataset = new DefaultXYZDataset();
+        Range range;
+
+        double data1[][] = { {1,1,1}, {1,1,1}, {1,2,3} };
+        dataset.addSeries("series1", data1);
+        range = r.findZBounds(dataset);
+        assertNotNull(range);
+        assertEquals(1d, range.getLowerBound());
+        assertEquals(3d, range.getUpperBound());
+
+        double data2[][] = { {1,1,1}, {1,1,1}, {-1,-2,-3} };
+        dataset.removeSeries("series1");
+        dataset.addSeries("series2", data2);
+        range = r.findZBounds(dataset);
+        assertNotNull(range);
+        assertEquals(-3d, range.getLowerBound());
+        assertEquals(-1d, range.getUpperBound());
+
+        double data3[][] = { {1,1,1}, {1,1,1}, {-1.2,2.9,3.8} };
+        dataset.removeSeries("series2");
+        dataset.addSeries("series3", data3);
+        range = r.findZBounds(dataset);
+        assertNotNull(range);
+        assertEquals(-1.2d, range.getLowerBound());
+        assertEquals(3.8d, range.getUpperBound());
+    }
+}
